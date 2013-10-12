86d391c0eed9f2086d3047aa93e9af9b58f9f532
hackooo <422642235@qq.com>
2013-10-13 00:14:57 +0800

    添加ImageCacheManager使用方法注释

--- src/cn/trinea/android/common/service/impl/ImageCacheManager.java
+++ src/cn/trinea/android/common/service/impl/ImageCacheManager.java
@@ -22,21 +22,40 @@ import android.view.animation.AlphaAnimation;
 import android.widget.ImageView;
 
 /**
-	 * 图片缓存管理，有两级缓存，第一级为 本地图片文件与内存的缓存，第二层结合ImageSDCardCacheManager，为网络与本地文件之间的缓存。
-	 *使用方法：导入这个类，或者直接作为内部类，获取图片的时候跟使用ImageCache一样，例如
-	 * ImageCacheManager imgCacheManager = new ImageCacheManager(context);
-	 * imgCacheManager.getImgCache().get(String url,View view);
-	 */
+ * @author maxiaohui hackooo@sina.cn
+ * 
+ * 图片缓存管理，有两级缓存，第一级为 本地图片文件与内存的缓存，第二层结合ImageSDCardCacheManager，为网络与本地文件之间的缓存。
+ * 使用方法：导入这个类，或者直接作为内部类，获取图片的时候跟使用ImageCache一样，例如：
+ * ImageCacheManager imgCacheManager = new ImageCacheManager(context);
+ *
+ * 参数的设置与原来的ImageCache和ImageSDCardCache一致(可以删除代码中的一些默认的设置)
+ * ImageCache IMAGE_CACHE = imgCacheManager。getImgCache();
+ * IMAGE_CACHE.setCacheFullRemoveType(new RemoveTypeLastUsedTimeFirst<Drawable>());
+ * IMAGE_CACHE.setHttpReadTimeOut(10000);
+ * IMAGE_CACHE.setOpenWaitingQueue(true);
+ * 。。。。。。
+ * ImageSDCardCache IMAGE_SD_CACHE = imgCacheManager.getImgSDCardCache();
+ * IMAGE_SD_CACHE.setCacheFullRemoveType(new RemoveTypeLastUsedTimeFirst<String>());
+ * IMAGE_SD_CACHE.setCacheFolder(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "TrineaAndroidCommon");
+ * 。。。。。。
+ * 获取图片的时候
+ * IMAGE_CACHE.get(String url,View view);
+ */
 	public class ImageCacheManager{
 		private  final ImageCache IMAGE_CACHE = new ImageCache(128);
 		private Context context;
+		private String TAG="ImageCacheManager";
 		
 		private ImageSDCardCacheManager imgSDCardCacheManager;
 
 		public ImageCacheManager(Context ctx){
 			context = ctx;
 			imgSDCardCacheManager = new ImageSDCardCacheManager();
-	        OnImageCallbackListener imageCallBack = new OnImageCallbackListener() {
+			IMAGE_CACHE.setCacheFullRemoveType(new RemoveTypeLastUsedTimeFirst<Drawable>());
+	        IMAGE_CACHE.setHttpReadTimeOut(10000);
+	        IMAGE_CACHE.setOpenWaitingQueue(true);
+	        IMAGE_CACHE.setValidTime(-1);
+	        IMAGE_CACHE.setOnImageCallbackListener(new OnImageCallbackListener() {
 	            private static final long serialVersionUID = 1L;
 	            @Override
 	            public void onImageLoaded(String imageUrl, Drawable imageDrawable, View view, boolean isInCache) {
@@ -48,20 +67,16 @@ import android.widget.ImageView;
 	                    }
 	                }
 	            }
-	        };
-	        IMAGE_CACHE.setOnImageCallbackListener(imageCallBack);
-	        IMAGE_CACHE.setCacheFullRemoveType(new RemoveTypeLastUsedTimeFirst<Drawable>());
-	        IMAGE_CACHE.setHttpReadTimeOut(10000);
-	        IMAGE_CACHE.setOpenWaitingQueue(true);
-	        IMAGE_CACHE.setValidTime(-1);
+	        });
+	        
 	        IMAGE_CACHE.setOnGetDataListener(new OnGetDataListener<String, Drawable>(){
 				private static final long serialVersionUID = -3015419170577770764L;
 				@Override
 				public CacheObject<Drawable> onGetData(String key) {
 			        	Drawable d = null;
 		                try {
-		                	String filepath;
-		                	//1.从SDCardCache的cache队列查询
+		                    String filepath;
+		                    //1.从SDCardCache的cache队列查询
 		                    CacheObject<String> co = imgSDCardCacheManager.getImgCache().get(key);
 		                    if(null != co){
 		                    	filepath= co.getData();
@@ -75,9 +90,9 @@ import android.widget.ImageView;
 	                    		Bitmap bm = BitmapFactory.decodeFile(filepath,null);
 			                    d = new BitmapDrawable(context.getResources(),bm);
 	                    	}
-		                    Log.d("test","file_path:"+filepath);
+		                    Log.d(TAG,"file_path:"+filepath);
 		                } catch (Exception e) {
-		                    Log.e("test", "get drawable exception, imageUrl is:" + key, e);
+		                    Log.e(TAG, "get drawable exception, imageUrl is:" + key, e);
 		                }
 		                return (d == null ? null : new CacheObject<Drawable>(d));
 				}});
@@ -90,10 +105,13 @@ import android.widget.ImageView;
 		public ImageCache getImgCache(){
 			return IMAGE_CACHE;
 		}
+		public ImageSDCardCache getImgSDCardCache(){
+			return imgSDCardCacheManager.getImgCache();
+		}
 	    
 		/**
 		 * 图片缓存管理，这一层只负责网络和本地图片之间的缓存
-		 * @author maxiaohui
+		 * @author maxiaohui  hackooo@sina.cn
 		 *
 		 */
 		private class ImageSDCardCacheManager {
@@ -110,17 +128,13 @@ import android.widget.ImageView;
 					private static final long serialVersionUID = 1L;
 					@Override
 					public CacheObject<String> onGetData(String key) {
-						/**
-		            	 * local file cache
-		            	 * @author maxiaohui
-		            	 */
-						String TAG = "test";
+						//1.先从本地文件查找；2.从网络获取
 		            	String imgPath =  IMAGE_SD_CACHE.getCacheFolder() + File.separator + IMAGE_SD_CACHE.getFileNameRule().getFileName(key);
 		            	File img = new File(imgPath);
 		            	if(img.exists()){
 		            		return new CacheObject<String>(imgPath);
 		            	}
-		            	//below is the same as ImageSDCardCache.getDefaultOnGetImageListener()
+		            	//下面与 ImageSDCardCache.getDefaultOnGetImageListener()内的实现相同
 		                String savePath = null;
 		                try {
 		                    InputStream stream = ImageUtils.getInputStreamFromUrl(key, IMAGE_SD_CACHE.getHttpReadTimeOut());
