495aff68e61a040f4455cafb6c0ef418a7922b6d
Trinea <jsjwgx_1989@126.com>
2013-11-01 14:12:01 +0800

    begin HttpCache.java

---
+++ src/cn/trinea/android/common/service/HttpCache.java
@@ -0,0 +1,10 @@
+package cn.trinea.android.common.service;
+
+/**
+ * HttpCache
+ * 
+ * @author <a href="http://www.trinea.cn" target="_blank">Trinea</a> 2013-11-1
+ */
+public class HttpCache {
+
+}
