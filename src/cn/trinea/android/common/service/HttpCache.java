2268546c607559007ff404fe11e3d8e65a47652a
Trinea <jsjwgx_1989@126.com>
2013-11-06 21:36:49 +0800

    deving HttpCache

--- src/cn/trinea/android/common/service/HttpCache.java
+++ src/cn/trinea/android/common/service/HttpCache.java
@@ -1,7 +1,15 @@
 package cn.trinea.android.common.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import android.content.Context;
+import cn.trinea.android.common.dao.impl.HttpCacheDaoImpl;
 import cn.trinea.android.common.entity.HttpRequest;
 import cn.trinea.android.common.entity.HttpResponse;
+import cn.trinea.android.common.util.HttpUtils;
+import cn.trinea.android.common.util.SqliteUtils;
+import cn.trinea.android.common.util.StringUtils;
 
 /**
  * HttpCache
@@ -10,15 +18,54 @@ import cn.trinea.android.common.entity.HttpResponse;
  */
 public class HttpCache {
 
-    public static HttpResponse httpGet(HttpRequest request) {
-        return null;
+    /** http memory cache **/
+    private Map<String, HttpResponse> cache;
+
+    public HttpCache(){
+        cache = new HashMap<String, HttpResponse>();
     }
 
-    public static HttpResponse httpGet(String httpUrl) {
-        return null;
+    /**
+     * get httpResponse whose type is type into memory as primary cache to improve performanceo
+     * 
+     * @param context
+     * @param type
+     */
+    public void initData(Context context, int type) {
+        if (context == null) {
+            throw new IllegalArgumentException("The context can not be null.");
+        }
+        this.cache = new HttpCacheDaoImpl(SqliteUtils.getInstance(context)).getHttpResponsesByType(type);
+        if (cache == null) {
+            cache = new HashMap<String, HttpResponse>();
+        }
     }
 
-    public static String httpGetString(String httpUrl) {
-        return null;
+    public HttpResponse httpGet(HttpRequest request) {
+        String url;
+        if (request == null || StringUtils.isEmpty(url = request.getUrl())) {
+            return null;
+        }
+
+        HttpResponse cacheResponse = getFromCache(url);
+        return cacheResponse == null ? HttpUtils.httpGet(request) : cacheResponse;
+    }
+
+    public HttpResponse httpGet(String httpUrl) {
+        HttpResponse cacheResponse = getFromCache(httpUrl);
+        return cacheResponse == null ? HttpUtils.httpGet(httpUrl) : cacheResponse;
+    }
+
+    public String httpGetString(String httpUrl) {
+        HttpResponse cacheResponse = getFromCache(httpUrl);
+        return cacheResponse == null ? HttpUtils.httpGetString(httpUrl) : cacheResponse.getResponseBody();
+    }
+
+    private HttpResponse getFromCache(String httpUrl) {
+        HttpResponse cacheResponse = cache.get(httpUrl);
+        if (cacheResponse == null) {
+            // get from db
+        }
+        return (cacheResponse == null || cacheResponse.isExpired()) ? null : cacheResponse;
     }
 }
