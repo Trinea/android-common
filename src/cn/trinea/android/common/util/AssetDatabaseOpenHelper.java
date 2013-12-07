bf0a6cc8cea1285b836d376754af7df5d23aa7ca
Trinea <jsjwgx_1989@126.com>
2013-12-07 10:13:22 +0800

    add AssetDatabaseOpenHelper
    can auto copy databse form assets to /data/data/package_name/databases and read or write it

---
+++ src/cn/trinea/android/common/util/AssetDatabaseOpenHelper.java
@@ -0,0 +1,95 @@
+package cn.trinea.android.common.util;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+
+/**
+ * AssetDatabaseOpenHelper
+ * <ul>
+ * <li>Auto copy databse form assets to /data/data/package_name/databases</li>
+ * <li>You can use it like {@link SQLiteDatabase}, use {@link #getWritableDatabase()} to create and/or open a database
+ * that will be used for reading and writing. use {@link #getReadableDatabase()} to create and/or open a database that
+ * will be used for reading only.</li>
+ * </ul>
+ * 
+ * @author <a href="http://www.trinea.cn" target="_blank">Trinea</a> 2013-12-5
+ */
+public class AssetDatabaseOpenHelper {
+
+    private Context context;
+    private String  databaseName;
+
+    public AssetDatabaseOpenHelper(Context context, String databaseName){
+        this.context = context;
+        this.databaseName = databaseName;
+    }
+
+    /**
+     * Create and/or open a database that will be used for reading and writing.
+     * 
+     * @return
+     * @throws RuntimeException if cannot copy database from assets
+     * @throws SQLiteException if the database cannot be opened
+     */
+    public synchronized SQLiteDatabase getWritableDatabase() {
+        File dbFile = context.getDatabasePath(databaseName);
+        if (!dbFile.exists()) {
+            try {
+                copyDatabase(dbFile);
+            } catch (IOException e) {
+                throw new RuntimeException("Error creating source database", e);
+            }
+        }
+
+        return SQLiteDatabase.openDatabase(dbFile.getPath(), null, SQLiteDatabase.OPEN_READWRITE);
+    }
+
+    /**
+     * Create and/or open a database that will be used for reading only.
+     * 
+     * @return
+     * @throws RuntimeException if cannot copy database from assets
+     * @throws SQLiteException if the database cannot be opened
+     */
+    public synchronized SQLiteDatabase getReadableDatabase() {
+        File dbFile = context.getDatabasePath(databaseName);
+        if (!dbFile.exists()) {
+            try {
+                copyDatabase(dbFile);
+            } catch (IOException e) {
+                throw new RuntimeException("Error creating source database", e);
+            }
+        }
+
+        return SQLiteDatabase.openDatabase(dbFile.getPath(), null, SQLiteDatabase.OPEN_READONLY);
+    }
+
+    /**
+     * @return the database name
+     */
+    public String getDatabaseName() {
+        return databaseName;
+    }
+
+    private void copyDatabase(File dbFile) throws IOException {
+        InputStream is = context.getAssets().open(databaseName);
+        FileUtils.makeDirs(dbFile.getAbsolutePath());
+        OutputStream os = new FileOutputStream(dbFile);
+
+        byte[] buffer = new byte[1024];
+        while (is.read(buffer) > 0) {
+            os.write(buffer);
+        }
+
+        os.flush();
+        os.close();
+        is.close();
+    }
+}
