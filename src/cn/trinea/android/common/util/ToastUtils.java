3f8a2c78cfcf77e03415bc454ecc5a7e1cc37965
Trinea <jsjwgx_1989@126.com>
2013-12-09 05:37:29 +0800

    add ToastUtils.java

---
+++ src/cn/trinea/android/common/util/ToastUtils.java
@@ -0,0 +1,44 @@
+package cn.trinea.android.common.util;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * ToastUtils
+ * 
+ * @author <a href="http://www.trinea.cn" target="_blank">Trinea</a> 2013-12-9
+ */
+public class ToastUtils {
+
+    public static void show(Context context, int resId) {
+        show(context, context.getResources().getText(resId), Toast.LENGTH_SHORT);
+    }
+
+    public static void show(Context context, int resId, int duration) {
+        show(context, context.getResources().getText(resId), duration);
+    }
+
+    public static void show(Context context, CharSequence text) {
+        show(context, text, Toast.LENGTH_SHORT);
+    }
+
+    public static void show(Context context, CharSequence text, int duration) {
+        Toast.makeText(context, text, duration).show();
+    }
+
+    public static void show(Context context, int resId, Object... args) {
+        show(context, String.format(context.getResources().getString(resId), args), Toast.LENGTH_SHORT);
+    }
+
+    public static void show(Context context, String format, Object... args) {
+        show(context, String.format(format, args), Toast.LENGTH_SHORT);
+    }
+
+    public static void show(Context context, int resId, int duration, Object... args) {
+        show(context, String.format(context.getResources().getString(resId), args), duration);
+    }
+
+    public static void show(Context context, String format, int duration, Object... args) {
+        show(context, String.format(format, args), duration);
+    }
+}
