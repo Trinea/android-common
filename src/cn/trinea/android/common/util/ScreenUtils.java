66a04e81006b006d17f265e471f267184ae4d435
Trinea <jsjwgx_1989@126.com>
2014-02-14 16:39:49 +0800

    add ScreenUtils.java

---
+++ src/cn/trinea/android/common/util/ScreenUtils.java
@@ -0,0 +1,30 @@
+package cn.trinea.android.common.util;
+
+import android.content.Context;
+
+/**
+ * ScreenUtils
+ * <ul>
+ * <strong>Convert between dp and sp</strong>
+ * <li>{@link ScreenUtils#dpToPx(Context, float)}</li>
+ * <li>{@link ScreenUtils#pxToDp(Context, float)}</li>
+ * </ul>
+ * 
+ * @author <a href="http://www.trinea.cn" target="_blank">Trinea</a> 2014-2-14
+ */
+public class ScreenUtils {
+
+    public static float dpToPx(Context context, float dp) {
+        if (context == null) {
+            return -1;
+        }
+        return dp * context.getResources().getDisplayMetrics().density;
+    }
+
+    public static float pxToDp(Context context, float px) {
+        if (context == null) {
+            return -1;
+        }
+        return px / context.getResources().getDisplayMetrics().density;
+    }
+}
