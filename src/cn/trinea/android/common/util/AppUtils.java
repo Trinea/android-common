7716826cb47b2be89bff9d5e329fbbbb5f73557e
Trinea <jsjwgx_1989@126.com>
2014-05-07 12:50:44 +0800

    Add AppUtils

---
+++ src/cn/trinea/android/common/util/AppUtils.java
@@ -0,0 +1,51 @@
+package cn.trinea.android.common.util;
+
+import java.util.List;
+
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
+import android.content.Context;
+
+/**
+ * AppUtils
+ * <ul>
+ * <li>{@link AppUtils#isNamedProcess(Context, String)}</li>
+ * </ul>
+ * 
+ * @author <a href="http://www.trinea.cn" target="_blank">Trinea</a> 2014-5-07
+ */
+public class AppUtils {
+
+    /**
+     * whether this process is named with processName
+     * 
+     * @param context
+     * @param processName
+     * @return <ul>
+     *         return whether this process is named with processName
+     *         <li>if context is null, return false</li>
+     *         <li>if {@link ActivityManager#getRunningAppProcesses()} is null, return false</li>
+     *         <li>if one process of {@link ActivityManager#getRunningAppProcesses()} is equal to processName, return
+     *         true, otherwise return false</li>
+     *         </ul>
+     */
+    public static boolean isNamedProcess(Context context, String processName) {
+        if (context == null) {
+            return false;
+        }
+
+        int pid = android.os.Process.myPid();
+        ActivityManager manager = (ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<RunningAppProcessInfo> processInfoList = manager.getRunningAppProcesses();
+        if (processInfoList == null) {
+            return true;
+        }
+
+        for (RunningAppProcessInfo processInfo : manager.getRunningAppProcesses()) {
+            if (processInfo.pid == pid && ObjectUtils.isEquals(processName, processInfo.processName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
