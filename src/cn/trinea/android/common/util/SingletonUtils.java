7917d5c50bd5d802097173faea554344730e2fb3
Trinea <jsjwgx_1989@126.com>
2014-05-28 20:57:48 +0800

    Add SingletonUtils

---
+++ src/cn/trinea/android/common/util/SingletonUtils.java
@@ -0,0 +1,25 @@
+package cn.trinea.android.common.util;
+
+/**
+ * Singleton helper class for lazily initialization.
+ * 
+ * @author <a href="http://www.trinea.cn/" target="_blank">Trinea</a>
+ * 
+ * @param <T>
+ */
+public abstract class SingletonUtils<T> {
+    private T instance;
+
+    protected abstract T newInstance();
+
+    public final T getInstance() {
+        if (instance == null) {
+            synchronized (SingletonUtils.class) {
+                if (instance == null) {
+                    instance = newInstance();
+                }
+            }
+        }
+        return instance;
+    }
+}
