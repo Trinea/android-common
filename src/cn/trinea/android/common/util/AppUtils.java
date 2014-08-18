d7420ee3f3d31ec8e9ef628197aeaa9b736e9125
Trinea <jsjwgx_1989@126.com>
2014-08-18 17:55:15 +0800

    Add isApplicationInBackground for AppUtils

--- src/cn/trinea/android/common/util/AppUtils.java
+++ src/cn/trinea/android/common/util/AppUtils.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningAppProcessInfo;
+import android.app.ActivityManager.RunningTaskInfo;
+import android.content.ComponentName;
 import android.content.Context;
 
 /**
@@ -43,6 +45,27 @@ public class AppUtils {
 
         for (RunningAppProcessInfo processInfo : manager.getRunningAppProcesses()) {
             if (processInfo.pid == pid && ObjectUtils.isEquals(processName, processInfo.processName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * whether application is in background
+     * <ul>
+     * <li>need use permission android.permission.GET_TASKS in Manifest.xml</li>
+     * </ul>
+     * 
+     * @param context
+     * @return if application is in background return true, otherwise return false
+     */
+    public static boolean isApplicationInBackground(Context context) {
+        ActivityManager am = (ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<RunningTaskInfo> taskList = am.getRunningTasks(1);
+        if (taskList != null && !taskList.isEmpty()) {
+            ComponentName topActivity = taskList.get(0).topActivity;
+            if (topActivity != null && !topActivity.getPackageName().equals(context.getPackageName())) {
                 return true;
             }
         }
