2879671aa3ecf802b7753062127429aa2fe68037
Trinea <jsjwgx_1989@126.com>
2014-11-05 17:45:48 +0800

    Add NetWorkUtils.java

---
+++ src/cn/trinea/android/common/util/NetWorkUtils.java
@@ -0,0 +1,101 @@
+package cn.trinea.android.common.util;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+
+/**
+ * NetWork Utils
+ * 
+ * @author <a href="http://www.trinea.cn" target="_blank">Trinea</a> 2014-11-03
+ */
+public class NetWorkUtils {
+
+    public static final String NETWORK_TYPE_WIFI       = "wifi";
+    public static final String NETWORK_TYPE_3G         = "eg";
+    public static final String NETWORK_TYPE_2G         = "2g";
+    public static final String NETWORK_TYPE_WAP        = "wap";
+    public static final String NETWORK_TYPE_UNKNOWN    = "unknown";
+    public static final String NETWORK_TYPE_DISCONNECT = "disconnect";
+
+    /**
+     * Get network type
+     * 
+     * @param context
+     * @return
+     */
+    public static String getNetWorkType(Context context) {
+        ConnectivityManager manager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo;
+        String type = NETWORK_TYPE_DISCONNECT;
+        if (manager == null || (networkInfo = manager.getActiveNetworkInfo()) == null) {
+            return type;
+        };
+
+        if (networkInfo.isConnected()) {
+            String typeName = networkInfo.getTypeName();
+            if ("WIFI".equalsIgnoreCase(typeName)) {
+                type = NETWORK_TYPE_WIFI;
+            } else if ("MOBILE".equalsIgnoreCase(typeName)) {
+                String proxyHost = android.net.Proxy.getDefaultHost();
+                type = TextUtils.isEmpty(proxyHost) ? (isFastMobileNetwork(context) ? NETWORK_TYPE_3G : NETWORK_TYPE_2G)
+                        : NETWORK_TYPE_WAP;
+            } else {
+                type = NETWORK_TYPE_UNKNOWN;
+            }
+        }
+        return type;
+    }
+
+    /**
+     * Whether is fast mobile network
+     * 
+     * @param context
+     * @return
+     */
+    private static boolean isFastMobileNetwork(Context context) {
+        TelephonyManager telephonyManager = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+        if (telephonyManager == null) {
+            return false;
+        }
+
+        switch (telephonyManager.getNetworkType()) {
+            case TelephonyManager.NETWORK_TYPE_1xRTT:
+                return false;
+            case TelephonyManager.NETWORK_TYPE_CDMA:
+                return false;
+            case TelephonyManager.NETWORK_TYPE_EDGE:
+                return false;
+            case TelephonyManager.NETWORK_TYPE_EVDO_0:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_EVDO_A:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_GPRS:
+                return false;
+            case TelephonyManager.NETWORK_TYPE_HSDPA:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_HSPA:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_HSUPA:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_UMTS:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_EHRPD:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_EVDO_B:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_HSPAP:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_IDEN:
+                return false;
+            case TelephonyManager.NETWORK_TYPE_LTE:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+                return false;
+            default:
+                return false;
+        }
+    }
+}
