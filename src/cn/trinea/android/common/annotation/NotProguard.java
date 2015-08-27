6ada6fe5783bc12c442472af9aedf8d5fce0a24e
Trinea <jsjwgx_1989@126.com>
2015-08-28 01:01:09 +0800

    Update NotProguard.java

--- src/cn/trinea/android/common/annotation/NotProguard.java
+++ src/cn/trinea/android/common/annotation/NotProguard.java
@@ -10,7 +10,6 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
 @Retention(RetentionPolicy.CLASS)
 @Target({ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.FIELD})
 public @interface NotProguard {
