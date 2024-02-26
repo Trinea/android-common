9d53f321e43842897c9c96393d42fef6b4965ed9
Trinea <trinea.cn@gmail.com>
2024-02-27 00:08:40 +0800

    execCommand support timeout

--- src/cn/trinea/android/common/util/ShellUtils.java
+++ src/cn/trinea/android/common/util/ShellUtils.java
@@ -1,10 +1,13 @@
-package cn.trinea.android.common.util;
+package cn.trinea.android.lib.util;
+
+import android.os.Build;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * ShellUtils
@@ -14,21 +17,27 @@ import java.util.List;
  * </ul>
  * <ul>
  * <strong>Execte command</strong>
+ * <li>{@link ShellUtils#execCommand(String)}</li>
+ * <li>{@link ShellUtils#execCommand(String, long)}</li>
  * <li>{@link ShellUtils#execCommand(String, boolean)}</li>
+ * <li>{@link ShellUtils#execCommand(String, boolean, long)}</li>
  * <li>{@link ShellUtils#execCommand(String, boolean, boolean)}</li>
+ * <li>{@link ShellUtils#execCommand(String, boolean, long, boolean)}</li>
  * <li>{@link ShellUtils#execCommand(List, boolean)}</li>
- * <li>{@link ShellUtils#execCommand(List, boolean, boolean)}</li>
+ * <li>{@link ShellUtils#execCommand(List, boolean, long)}</li>
+ * <li>{@link ShellUtils#execCommand(List, boolean, long, boolean)}</li>
  * <li>{@link ShellUtils#execCommand(String[], boolean)}</li>
- * <li>{@link ShellUtils#execCommand(String[], boolean, boolean)}</li>
+ * <li>{@link ShellUtils#execCommand(String[], boolean, long)}</li>
+ * <li>{@link ShellUtils#execCommand(String[], boolean, long, boolean)}</li>
  * </ul>
- * 
- * @author <a href="http://www.trinea.cn" target="_blank">Trinea</a> 2013-5-16
+ *
+ * @author <a href="https://www.trinea.cn" target="_blank">Trinea</a> 2013-5-16
  */
 public class ShellUtils {
 
-    public static final String COMMAND_SU       = "su";
-    public static final String COMMAND_SH       = "sh";
-    public static final String COMMAND_EXIT     = "exit\n";
+    public static final String COMMAND_SU = "su";
+    public static final String COMMAND_SH = "sh";
+    public static final String COMMAND_EXIT = "exit\n";
     public static final String COMMAND_LINE_END = "\n";
 
     private ShellUtils() {
@@ -37,88 +46,118 @@ public class ShellUtils {
 
     /**
      * check whether has root permission
-     * 
+     *
      * @return
      */
     public static boolean checkRootPermission() {
-        return execCommand("echo root", true, false).result == 0;
+        return execCommand("echo root", true, 5000, false).result == 0;
+    }
+
+    public static CommandResult execCommand(String command) {
+        return execCommand(new String[]{command}, false, -1, true);
+    }
+
+    public static CommandResult execCommand(String command, long timeoutInMills) {
+        return execCommand(new String[]{command}, false, timeoutInMills, true);
+    }
+
+    public static CommandResult execCommand(String command, boolean isRoot) {
+        return execCommand(new String[]{command}, isRoot, -1, true);
     }
 
     /**
      * execute shell command, default return result msg
-     * 
-     * @param command command
-     * @param isRoot whether need to run with root
+     *
+     * @param command        command
+     * @param isRoot         whether need to run with root
+     * @param timeoutInMills timeout in millSeconds, -1 means no timeout, if bigger than 0, you must make sure Android Version is not smaller than Android O
      * @return
-     * @see ShellUtils#execCommand(String[], boolean, boolean)
+     * @see ShellUtils#execCommand(String[], boolean, long, boolean)
      */
-    public static CommandResult execCommand(String command, boolean isRoot) {
-        return execCommand(new String[] {command}, isRoot, true);
+    public static CommandResult execCommand(String command, boolean isRoot, long timeoutInMills) {
+        return execCommand(new String[]{command}, isRoot, timeoutInMills, true);
     }
 
-    /**
-     * execute shell commands, default return result msg
-     * 
-     * @param commands command list
-     * @param isRoot whether need to run with root
-     * @return
-     * @see ShellUtils#execCommand(String[], boolean, boolean)
-     */
     public static CommandResult execCommand(List<String> commands, boolean isRoot) {
-        return execCommand(commands == null ? null : commands.toArray(new String[] {}), isRoot, true);
+        return execCommand(commands == null ? null : commands.toArray(new String[]{}), isRoot, -1, true);
     }
 
     /**
      * execute shell commands, default return result msg
-     * 
-     * @param commands command array
-     * @param isRoot whether need to run with root
+     *
+     * @param commands       command list
+     * @param isRoot         whether need to run with root
+     * @param timeoutInMills timeout in millSeconds, -1 means no timeout, if bigger than 0, you must make sure Android Version is not smaller than Android O
      * @return
-     * @see ShellUtils#execCommand(String[], boolean, boolean)
+     * @see ShellUtils#execCommand(String[], boolean, long, boolean)
      */
+    public static CommandResult execCommand(List<String> commands, boolean isRoot, long timeoutInMills) {
+        return execCommand(commands == null ? null : commands.toArray(new String[]{}), isRoot, timeoutInMills, true);
+    }
+
     public static CommandResult execCommand(String[] commands, boolean isRoot) {
-        return execCommand(commands, isRoot, true);
+        return execCommand(commands, isRoot, -1, true);
+    }
+
+    /**
+     * execute shell commands, default return result msg
+     *
+     * @param commands       command array
+     * @param isRoot         whether need to run with root
+     * @param timeoutInMills timeout in millSeconds, -1 means no timeout, if bigger than 0, you must make sure Android Version is not smaller than Android O
+     * @return
+     * @see ShellUtils#execCommand(String[], boolean, long, boolean)
+     */
+    public static CommandResult execCommand(String[] commands, boolean isRoot, long timeoutInMills) {
+        return execCommand(commands, isRoot, timeoutInMills, true);
+    }
+
+    public static CommandResult execCommand(String command, boolean isRoot, boolean isNeedResultMsg) {
+        return execCommand(new String[]{command}, isRoot, -1, isNeedResultMsg);
     }
 
     /**
      * execute shell command
-     * 
-     * @param command command
-     * @param isRoot whether need to run with root
+     *
+     * @param command         command
+     * @param isRoot          whether need to run with root
+     * @param timeoutInMills  timeout in millSeconds, -1 means no timeout, if bigger than 0, you must make sure Android Version is not smaller than Android O
      * @param isNeedResultMsg whether need result msg
      * @return
-     * @see ShellUtils#execCommand(String[], boolean, boolean)
+     * @see ShellUtils#execCommand(String[], boolean, long, boolean)
      */
-    public static CommandResult execCommand(String command, boolean isRoot, boolean isNeedResultMsg) {
-        return execCommand(new String[] {command}, isRoot, isNeedResultMsg);
+    public static CommandResult execCommand(String command, boolean isRoot, long timeoutInMills, boolean isNeedResultMsg) {
+        return execCommand(new String[]{command}, isRoot, timeoutInMills, isNeedResultMsg);
     }
 
     /**
      * execute shell commands
-     * 
-     * @param commands command list
-     * @param isRoot whether need to run with root
+     *
+     * @param commands        command list
+     * @param isRoot          whether need to run with root
+     * @param timeoutInMills  timeout in millSeconds, -1 means no timeout, if bigger than 0, you must make sure Android Version is not smaller than Android O
      * @param isNeedResultMsg whether need result msg
      * @return
-     * @see ShellUtils#execCommand(String[], boolean, boolean)
+     * @see ShellUtils#execCommand(String[], boolean, long, boolean)
      */
-    public static CommandResult execCommand(List<String> commands, boolean isRoot, boolean isNeedResultMsg) {
-        return execCommand(commands == null ? null : commands.toArray(new String[] {}), isRoot, isNeedResultMsg);
+    public static CommandResult execCommand(List<String> commands, boolean isRoot, long timeoutInMills, boolean isNeedResultMsg) {
+        return execCommand(commands == null ? null : commands.toArray(new String[]{}), isRoot, timeoutInMills, isNeedResultMsg);
     }
 
     /**
      * execute shell commands
-     * 
-     * @param commands command array
-     * @param isRoot whether need to run with root
+     *
+     * @param commands        command array
+     * @param isRoot          whether need to run with root
+     * @param timeoutInMills  timeout in millSeconds, -1 means no timeout, if bigger than 0, you must make sure Android Version is not smaller than Android O
      * @param isNeedResultMsg whether need result msg
      * @return <ul>
-     *         <li>if isNeedResultMsg is false, {@link CommandResult#successMsg} is null and
-     *         {@link CommandResult#errorMsg} is null.</li>
-     *         <li>if {@link CommandResult#result} is -1, there maybe some excepiton.</li>
-     *         </ul>
+     * <li>if isNeedResultMsg is false, {@link CommandResult#successMsg} is null and
+     * {@link CommandResult#errorMsg} is null.</li>
+     * <li>if {@link CommandResult#result} is -1, there maybe some excepiton.</li>
+     * </ul>
      */
-    public static CommandResult execCommand(String[] commands, boolean isRoot, boolean isNeedResultMsg) {
+    public static CommandResult execCommand(String[] commands, boolean isRoot, long timeoutInMills, boolean isNeedResultMsg) {
         int result = -1;
         if (commands == null || commands.length == 0) {
             return new CommandResult(result, null, null);
@@ -131,6 +170,7 @@ public class ShellUtils {
         StringBuilder errorMsg = null;
 
         DataOutputStream os = null;
+        boolean isTimeout = false;
         try {
             process = Runtime.getRuntime().exec(isRoot ? COMMAND_SU : COMMAND_SH);
             os = new DataOutputStream(process.getOutputStream());
@@ -147,7 +187,14 @@ public class ShellUtils {
             os.writeBytes(COMMAND_EXIT);
             os.flush();
 
-            result = process.waitFor();
+            if (timeoutInMills <= 0) {
+                result = process.waitFor();
+            } else {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    isTimeout = process.waitFor(timeoutInMills, TimeUnit.MILLISECONDS);
+                    result = isTimeout ? -1 : 0;
+                }
+            }
             // get command result
             if (isNeedResultMsg) {
                 successMsg = new StringBuilder();
@@ -156,10 +203,10 @@ public class ShellUtils {
                 errorResult = new BufferedReader(new InputStreamReader(process.getErrorStream()));
                 String s;
                 while ((s = successResult.readLine()) != null) {
-                    successMsg.append(s);
+                    successMsg.append(s).append(COMMAND_LINE_END);
                 }
                 while ((s = errorResult.readLine()) != null) {
-                    errorMsg.append(s);
+                    errorMsg.append(s).append(COMMAND_LINE_END);
                 }
             }
         } catch (IOException e) {
@@ -185,6 +232,9 @@ public class ShellUtils {
                 process.destroy();
             }
         }
+        if (isTimeout && errorMsg.length() == 0) {
+            errorMsg.append("Timeout, possibly due to lack of permissions.");
+        }
         return new CommandResult(result, successMsg == null ? null : successMsg.toString(), errorMsg == null ? null
                 : errorMsg.toString());
     }
@@ -197,16 +247,22 @@ public class ShellUtils {
      * <li>{@link CommandResult#successMsg} means success message of command result</li>
      * <li>{@link CommandResult#errorMsg} means error message of command result</li>
      * </ul>
-     * 
-     * @author <a href="http://www.trinea.cn" target="_blank">Trinea</a> 2013-5-16
+     *
+     * @author <a href="https://www.trinea.cn" target="_blank">Trinea</a> 2013-5-16
      */
     public static class CommandResult {
 
-        /** result of command **/
-        public int    result;
-        /** success message of command result **/
+        /**
+         * result of command
+         **/
+        public int result;
+        /**
+         * success message of command result
+         **/
         public String successMsg;
-        /** error message of command result **/
+        /**
+         * error message of command result
+         **/
         public String errorMsg;
 
         public CommandResult(int result) {
@@ -217,6 +273,13 @@ public class ShellUtils {
             this.result = result;
             this.successMsg = successMsg;
             this.errorMsg = errorMsg;
+        }
+
+        @Override
+        public String toString() {
+            return "result is: " + result +
+                    "\nsuccessMsg is: '" + successMsg + "'" +
+                    "\nerrorMsg is: '" + errorMsg + "'";
         }
     }
 }
